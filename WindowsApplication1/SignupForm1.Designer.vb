98b3cebd431723057208036f96e9613fb9ba070e
evilbefall <naveengupta10001@gmail.com>
2019-02-01 23:00:57 +0530

    sign up form added

---
+++ WindowsApplication1/SignupForm1.Designer.vb
@@ -0,0 +1,153 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+<Global.System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Naming", "CA1726")> _
+Partial Class SignupForm1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+    Friend WithEvents LogoPictureBox As System.Windows.Forms.PictureBox
+    Friend WithEvents lblUsername As System.Windows.Forms.Label
+    Friend WithEvents lblPassword As System.Windows.Forms.Label
+    Friend WithEvents txtUsername As System.Windows.Forms.TextBox
+    Friend WithEvents txtPassword As System.Windows.Forms.TextBox
+    Friend WithEvents OK As System.Windows.Forms.Button
+    Friend WithEvents Cancel As System.Windows.Forms.Button
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(SignupForm1))
+        Me.LogoPictureBox = New System.Windows.Forms.PictureBox()
+        Me.lblUsername = New System.Windows.Forms.Label()
+        Me.lblPassword = New System.Windows.Forms.Label()
+        Me.txtUsername = New System.Windows.Forms.TextBox()
+        Me.txtPassword = New System.Windows.Forms.TextBox()
+        Me.OK = New System.Windows.Forms.Button()
+        Me.Cancel = New System.Windows.Forms.Button()
+        Me.txtConfirmPassword = New System.Windows.Forms.TextBox()
+        Me.lblConfirmPassword = New System.Windows.Forms.Label()
+        CType(Me.LogoPictureBox, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'LogoPictureBox
+        '
+        Me.LogoPictureBox.Image = CType(resources.GetObject("LogoPictureBox.Image"), System.Drawing.Image)
+        Me.LogoPictureBox.Location = New System.Drawing.Point(0, 0)
+        Me.LogoPictureBox.Name = "LogoPictureBox"
+        Me.LogoPictureBox.Size = New System.Drawing.Size(165, 263)
+        Me.LogoPictureBox.TabIndex = 0
+        Me.LogoPictureBox.TabStop = False
+        '
+        'lblUsername
+        '
+        Me.lblUsername.Location = New System.Drawing.Point(172, 24)
+        Me.lblUsername.Name = "lblUsername"
+        Me.lblUsername.Size = New System.Drawing.Size(220, 23)
+        Me.lblUsername.TabIndex = 0
+        Me.lblUsername.Text = "&User name"
+        Me.lblUsername.TextAlign = System.Drawing.ContentAlignment.MiddleLeft
+        '
+        'lblPassword
+        '
+        Me.lblPassword.Location = New System.Drawing.Point(172, 81)
+        Me.lblPassword.Name = "lblPassword"
+        Me.lblPassword.Size = New System.Drawing.Size(220, 23)
+        Me.lblPassword.TabIndex = 2
+        Me.lblPassword.Text = "&Password"
+        Me.lblPassword.TextAlign = System.Drawing.ContentAlignment.MiddleLeft
+        '
+        'txtUsername
+        '
+        Me.txtUsername.Location = New System.Drawing.Point(174, 44)
+        Me.txtUsername.Name = "txtUsername"
+        Me.txtUsername.Size = New System.Drawing.Size(246, 22)
+        Me.txtUsername.TabIndex = 1
+        '
+        'txtPassword
+        '
+        Me.txtPassword.Location = New System.Drawing.Point(174, 101)
+        Me.txtPassword.Name = "txtPassword"
+        Me.txtPassword.PasswordChar = Global.Microsoft.VisualBasic.ChrW(42)
+        Me.txtPassword.Size = New System.Drawing.Size(246, 22)
+        Me.txtPassword.TabIndex = 3
+        '
+        'OK
+        '
+        Me.OK.Location = New System.Drawing.Point(175, 217)
+        Me.OK.Name = "OK"
+        Me.OK.Size = New System.Drawing.Size(94, 23)
+        Me.OK.TabIndex = 4
+        Me.OK.Text = "&OK"
+        '
+        'Cancel
+        '
+        Me.Cancel.DialogResult = System.Windows.Forms.DialogResult.Cancel
+        Me.Cancel.Location = New System.Drawing.Point(326, 217)
+        Me.Cancel.Name = "Cancel"
+        Me.Cancel.Size = New System.Drawing.Size(94, 23)
+        Me.Cancel.TabIndex = 5
+        Me.Cancel.Text = "&Cancel"
+        '
+        'txtConfirmPassword
+        '
+        Me.txtConfirmPassword.Location = New System.Drawing.Point(174, 158)
+        Me.txtConfirmPassword.Name = "txtConfirmPassword"
+        Me.txtConfirmPassword.PasswordChar = Global.Microsoft.VisualBasic.ChrW(42)
+        Me.txtConfirmPassword.Size = New System.Drawing.Size(246, 22)
+        Me.txtConfirmPassword.TabIndex = 7
+        '
+        'lblConfirmPassword
+        '
+        Me.lblConfirmPassword.Location = New System.Drawing.Point(172, 138)
+        Me.lblConfirmPassword.Name = "lblConfirmPassword"
+        Me.lblConfirmPassword.Size = New System.Drawing.Size(220, 23)
+        Me.lblConfirmPassword.TabIndex = 6
+        Me.lblConfirmPassword.Text = "&Confirm Password"
+        Me.lblConfirmPassword.TextAlign = System.Drawing.ContentAlignment.MiddleLeft
+        '
+        'SignupForm1
+        '
+        Me.AcceptButton = Me.OK
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.CancelButton = Me.Cancel
+        Me.ClientSize = New System.Drawing.Size(462, 261)
+        Me.Controls.Add(Me.txtConfirmPassword)
+        Me.Controls.Add(Me.lblConfirmPassword)
+        Me.Controls.Add(Me.Cancel)
+        Me.Controls.Add(Me.OK)
+        Me.Controls.Add(Me.txtPassword)
+        Me.Controls.Add(Me.txtUsername)
+        Me.Controls.Add(Me.lblPassword)
+        Me.Controls.Add(Me.lblUsername)
+        Me.Controls.Add(Me.LogoPictureBox)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog
+        Me.MaximizeBox = False
+        Me.MinimizeBox = False
+        Me.Name = "SignupForm1"
+        Me.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent
+        Me.Text = "SignupForm1"
+        CType(Me.LogoPictureBox, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents txtConfirmPassword As System.Windows.Forms.TextBox
+    Friend WithEvents lblConfirmPassword As System.Windows.Forms.Label
+
+End Class
