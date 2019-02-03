d48326c53e81b9c8ff0dc52bfaa92fe873c150b8
Rishi Pathak <33569110+rishipathak6@users.noreply.github.com>
2019-02-03 16:18:12 +0530

    Updated the Signup UI
    Arranged the labels, text boxes, buttons
    added IITG photo
    Added logo and name to sign up form

---
+++ GuestHouseManagement-master/WindowsApplication1/Form1.Designer.vb
@@ -0,0 +1,64 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.btnSignUp = New System.Windows.Forms.Button()
+        Me.btnLogin = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'btnSignUp
+        '
+        Me.btnSignUp.BackColor = System.Drawing.Color.Transparent
+        Me.btnSignUp.Location = New System.Drawing.Point(787, 2)
+        Me.btnSignUp.Name = "btnSignUp"
+        Me.btnSignUp.Size = New System.Drawing.Size(191, 49)
+        Me.btnSignUp.TabIndex = 0
+        Me.btnSignUp.Text = "Sign Up"
+        Me.btnSignUp.UseVisualStyleBackColor = False
+        '
+        'btnLogin
+        '
+        Me.btnLogin.BackColor = System.Drawing.Color.Transparent
+        Me.btnLogin.Location = New System.Drawing.Point(787, 57)
+        Me.btnLogin.Name = "btnLogin"
+        Me.btnLogin.Size = New System.Drawing.Size(191, 49)
+        Me.btnLogin.TabIndex = 1
+        Me.btnLogin.Text = "Login"
+        Me.btnLogin.UseVisualStyleBackColor = False
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(981, 530)
+        Me.Controls.Add(Me.btnLogin)
+        Me.Controls.Add(Me.btnSignUp)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents btnSignUp As System.Windows.Forms.Button
+    Friend WithEvents btnLogin As System.Windows.Forms.Button
+
+End Class
---
+++ GuestHouseManagement-master/WindowsApplication1/SignupForm1.vb
@@ -0,0 +1,72 @@
+﻿Imports System.Drawing.Drawing2D
+Public Class SignupForm1
+    Dim str As String
+    ' TODO: Insert code to perform custom authentication using the provided username and password 
+    ' (See http://go.microsoft.com/fwlink/?LinkId=35339).  
+    ' The custom principal can then be attached to the current thread's principal as follows: 
+    '     My.User.CurrentPrincipal = CustomPrincipal
+    ' where CustomPrincipal is the IPrincipal implementation used to perform authentication. 
+    ' Subsequently, My.User will return identity information encapsulated in the CustomPrincipal object
+    ' such as the username, display name, etc.
+
+    Private Sub OK_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles OK.Click
+        If txtCaptcha.Text = str Then
+            UserTableBindingSource.EndEdit()
+            UserTableTableAdapter.Update(GuestHouseDataSet.userTable)
+            MsgBox("Register Success")
+            Me.Close()
+        Else
+            MsgBox("Register Failed")
+            btnRefresh.PerformClick()
+            txtCaptcha.Clear()
+        End If
+    End Sub
+
+    Private Sub Cancel_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Cancel.Click
+        UserTableBindingSource.RemoveCurrent()
+        Me.Close()
+    End Sub
+
+    Private Sub btnRefresh_Click(sender As Object, e As EventArgs) Handles btnRefresh.Click
+        Dim NumCaptcha As String = "123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+        str = ""
+        Dim R As New Random
+        For i As Integer = 0 To 5
+            str = str + NumCaptcha(R.Next(0, 60))
+        Next
+        Dim b As New Bitmap(141, 36, Imaging.PixelFormat.Format32bppArgb)
+        Dim g As Graphics = Graphics.FromImage(b)
+        Dim Hb As New HatchBrush(HatchStyle.DottedDiamond, Color.FromArgb(255, 128, 0), Color.Black)
+        g.DrawString(str, New Font("Monotype Corsiva", 16, FontStyle.Strikeout, GraphicsUnit.Point), Brushes.White, 5, 5)
+        picCaptcha.Image = b
+    End Sub
+
+    Private Sub picCaptcha_Click(sender As Object, e As EventArgs) Handles picCaptcha.Click
+
+    End Sub
+
+    Private Sub SignupForm1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        'TODO: This line of code loads data into the 'GuestHouseDataSet.userTable' table. You can move, or remove it, as needed.
+        Me.UserTableTableAdapter.Fill(Me.GuestHouseDataSet.userTable)
+        UserTableBindingSource.AddNew()
+        Dim NumCaptcha As String = "123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+        str = ""
+        Dim R As New Random
+        For i As Integer = 0 To 5
+            str = str + NumCaptcha(R.Next(0, 60))
+        Next
+        Dim b As New Bitmap(141, 36, Imaging.PixelFormat.Format32bppArgb)
+        Dim g As Graphics = Graphics.FromImage(b)
+        Dim Hb As New HatchBrush(HatchStyle.DottedDiamond, Color.FromArgb(255, 128, 0), Color.Black)
+        g.DrawString(str, New Font("Monotype Corsiva", 16, FontStyle.Strikeout, GraphicsUnit.Point), Brushes.White, 5, 5)
+        picCaptcha.Image = b
+    End Sub
+
+    Private Sub txtUsername_TextChanged(sender As Object, e As EventArgs) Handles txtUsername.TextChanged
+
+    End Sub
+
+    Private Sub txtConfirmPassword_TextChanged(sender As Object, e As EventArgs) Handles txtConfirmPassword.TextChanged
+
+    End Sub
+End Class
