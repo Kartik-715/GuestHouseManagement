fb0ddd931ed1e9a27f8435eeeb717ad9fa609d32
evilbefall <naveengupta10001@gmail.com>
2019-02-05 02:04:47 +0530

    Form1 responsive

--- WindowsApplication1/Form1.vb
+++ WindowsApplication1/Form1.vb
@@ -41,8 +41,4 @@
         Me.WindowState = FormWindowState.Normal
         Me.WindowState = FormWindowState.Maximized
     End Sub
-
-    Private Sub Label1_Click(sender As Object, e As EventArgs) Handles Label1.Click
-
-    End Sub
 End Class
